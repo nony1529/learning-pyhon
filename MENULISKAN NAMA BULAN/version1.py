bb7f42b682efb8a53813aed3b993acd1eb581ce1
= <=>
2024-11-24 12:47:53 +0700

    MENULISKAN NAMA TAHUN

---
+++ MENULISKAN NAMA BULAN/version1.py
@@ -0,0 +1,32 @@
+print("=" * 40)
+print("\tMenentukan Nama Bulan")
+print("=" * 40)
+
+i = int(input("Masukan Angka Bulang (1-12) = "))
+
+if i == 1:
+    print("Januari")
+elif i == 2:
+    print("Februari")
+elif i == 3:
+    print("Maret")
+elif i == 4:
+    print("April")
+elif i == 5:
+    print("Mei")
+elif i == 6:
+    print("Juni")
+elif i == 7:
+    print("Juli")
+elif i == 8:
+    print("Agustus")
+elif i == 9:
+    print("September")
+elif i == 10:
+    print("Oktober")
+elif i == 11:
+    print("November")
+elif i == 12:
+    print("Desember")
+else:
+    print("Tahun Tidak Tersedia")
---
+++ MENULISKAN NAMA BULAN/version2.py
@@ -0,0 +1,3 @@
+bulan = ["Januari","Februari","Maret","April","Mei","Juni","Auli","Agustus","September","Oktober","November","Desember"]
+for i,x in enumerate (bulan) :
+    print(f"Bulan ke-{i+1} yaitu {x}")
