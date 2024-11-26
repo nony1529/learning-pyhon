f68d4678d965177ce494fe81ebd9a719c0fcdd26
= <=>
2024-11-26 11:44:22 +0700

    OPERASI HITUNG

--- OPERASI HITUNG/kpk.py
+++ OPERASI HITUNG/kpk.py
@@ -2,22 +2,22 @@ print("=" * 46)
 print("\tMembuat Program Mencari KPK")
 print("=" * 46)
 
-def angka_kpk (x, y):
-    kpk = max(x, y)
+def cari_kpk (a, b):
+    kpk = max(a, b)
     cari = kpk
 
-    while (kpk == max(x, y)):
-        if (cari % x == 0) and (cari% y == 0):
+    while (kpk == max(a, b)):
+        if (cari % a == 0) and (cari % b == 0):
             kpk = cari
         cari += 1
 
-        return kpk
+    return kpk
 
-def main ():
-    angka1 = int(input("Masukan Bilangan Pertama\t= "))
-    angka2 = int(input("Masukan Bilangan Kedua\t\t= "))
+def main():
+    bilangan1 = int(input("Masuka Bilangan Pertama = "))
+    bilangan2 = int(input("Masukan Bilangan Kedua = "))
 
-    hasil_kpk = angka_kpk(angka1, angka2)
-    print("KPK dari", angka1, "dan", angka2, "adalah\t=",hasil_kpk)
+    hasil_kpk = cari_kpk(bilangan1, bilangan2)
+    print("KPK dari", bilangan1, "dan", bilangan2, "adalah", hasil_kpk)
 
 main()
